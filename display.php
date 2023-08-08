b554f99456b388c1f330ff90be76acaf423c0f82
thequackingduck <justinlomodcorpuz0626@gmail.com>
2023-08-08 17:01:53 +0800

    second commit

--- display.php
+++ display.php
@@ -9,7 +9,7 @@ include 'db_connect.php';
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
 
 	<title>Car Booking System</title>
 
@@ -22,14 +22,7 @@ include 'db_connect.php';
 	<!-- Custom stlylesheet -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 	<link type="text/css" rel="stylesheet" href="css/style_display.css" />
-    
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
 
 </head>
 
